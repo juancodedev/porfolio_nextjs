62551dfd0ea8ae44ad49faa5ed5458edaf99da31
Juan Muñoz <cl.jmunoz@gmail.com>
2024-09-10 19:15:24 -0300

    SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <SpeedInsights/>
       <Analytics />
       <body className={jetbrainsMono.variable}>
         <Header />
