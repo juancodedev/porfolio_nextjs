f09f5efab9777ba4dfebf7b0da578d45a169ad09
Juan Muñoz <cl.jmunoz@gmail.com>
2026-07-26 15:50:38 -0400

    feat: add Google Tag Manager integration for enhanced tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NMR8WGWX');`,
+          }}
+        />
       <body className={`${jetbrainsMono.variable} ${inter.variable} ${sourceSerif4.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMR8WGWX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <SpeedInsights />
         <Analytics />
         <Header />
